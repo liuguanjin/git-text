d954d4f80a66244c8e97dba4f968dc131693aa06
liuguanjin <1060086991@qq.com>
2019-12-01 10:47:23 +0800

    新手web个人简历制作

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper('.swiper-container', {
+    direction:'vertical',
+    pagination: {
+      	el: '.swiper-pagination',
+      	clickable: true,
+    },
+    mousewheel:true,
+});
